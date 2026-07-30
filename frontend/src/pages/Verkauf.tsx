12270645b032368586e4b34d1bc27781f0d4fb0f
Erik Sauer <erik.pascal.sauer@gmx.de>
2026-07-30 16:39:03 +0200

    liniting fix

--- frontend/src/pages/Verkauf.tsx
+++ frontend/src/pages/Verkauf.tsx
@@ -617,8 +617,7 @@ function SwipeKorbZeile({ children, onRemove }: { children: ReactNode; onRemove:
   function up(e: PointerEvent<HTMLDivElement>) {
     try {
       e.currentTarget.releasePointerCapture(e.pointerId);
-      // eslint-disable-next-line no-empty
-    } catch (err) { /* Fehler hier ist nicht kritisch, Geste wird trotzdem beendet */ }
+    } catch (err) { /* Der Fehler beim Freigeben des Pointers ist hier nicht kritisch, die Geste wird trotzdem beendet. */ }
 
     if (offset < -62) {
       onRemove();
